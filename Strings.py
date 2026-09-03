675e8005b344e2d49c74ac9e99484f84ac354b49
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-03 16:49:31 +0530

    I change the paragraph

--- Strings.py
+++ Strings.py
@@ -4,12 +4,7 @@ print(name[0])
 print(name[1])
 print(name[2])
 print(name[3])
-Engineering = "Engineering Only can be done by boys! " \
-"beacause they use their mind for crtical thinking and all, but girls are also doing engineeing just becuase " \
-"they can divert mind of innocent boys, and make critical thinker purush mitra for life long sustainablility " \
-"As we know girls can not do anything , they always depend on boys for any work, Those faltu jokes can only handle by boys " \
-""
-
+Engineering = "Computer Science"
 for i in range(len(name)):
     print(name[i])
 
