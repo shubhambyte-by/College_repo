88abec5ffe81cfc2cbb938260403e86e997dbbf5
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-10 16:49:37 +0530

    your comment

--- swapping_value.py
+++ swapping_value.py
@@ -1,7 +1,7 @@
 # Swapping of values:
 
-a = 30
-b = 40
+a = 40
+b = 60
 a, b = b, a
 print("a =", a)
 print("b =", b)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,3 +2,5 @@ if (10 + 5) * 2 > 20 and 4 != 5:
     print("True")
 else:
     print("False")   
+
+print("Hello World")
