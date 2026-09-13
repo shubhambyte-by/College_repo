687aee3494082046fdd2a6a23ac9d7e97b1023c5
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-13 09:37:17 +0530

    update

---
+++ divisible.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number  : "))
+
+i = 0
+for i in range(i,n):
+    if (i%5==0 and i%10==0):
+        print(i)
+
+    
+    i = i + 1 
+
+git status
+git add .
+git commit -m "update"
+git push origin main    
