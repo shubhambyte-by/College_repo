ae64c2812d3a44019d3000464c1e57bb6cfe208f
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-13 23:13:25 +0530

    update

--- Boolean_operators.py
+++ Boolean_operators.py
@@ -1,18 +1,3 @@
-# Using IF - ELIF - ELSE
-
-# marks = int(input("Enter marks: "))
-#
-# if marks >= 90:
-#     print("Excellent")
-#
-# elif marks >= 60:
-#     print("Good")
-# elif marks < 40:
-#     print("fail")
-# else:
-#     print("Need improvement")
-
-
 #creating a simple calculator
 
 # a = float(input("Enter first number: "))
---
+++ creatingCalc.py
@@ -0,0 +1,18 @@
+#creating a simple calculator
+
+a = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+b = float(input("Enter second number: "))
+
+if op == "+":
+    print("Result =", a + b)
+elif op == "-":
+    print("Result =", a - b)
+elif op == "*":
+    print("Result =", a * b)
+elif op == "/":
+    print("Result =", a / b)
+else:
+    print("Invalid operator")
+
+    
