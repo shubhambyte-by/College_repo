ce642d3323b0dc65d5bd25a5006111809434674e
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-13 23:23:24 +0530

    update

--- Boolean_operators.py
+++ Boolean_operators.py
@@ -1,15 +1,3 @@
-# largest number in a list
-
-# numbers = [10, 25, 7, 42, 18]
-# largest = numbers[0]
-#
-# for n in numbers:
-#     if n > largest:
-#         largest = n
-#
-# print("Largest =", largest)
-
-
 # Break pass
 
 # for i in range(1, 101):
---
+++ Break.py
@@ -0,0 +1,5 @@
+# Break
+for i in range(1, 101):
+    if i == 60:
+        break
+    print(i)
