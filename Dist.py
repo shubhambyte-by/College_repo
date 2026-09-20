57dd489445d762e4e5140dbae6ac9f5b4bdfce13
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-20 11:13:24 +0530

    update

--- Dist.py
+++ Dist.py
@@ -1,21 +1,34 @@
 # Dictionaries:
 
-student = {
-    "name": "Rahul",
-    "age": 20,
-    "course": "B.Tech IT"
-}
+# student = {
+#     "name": "Rahul",
+#     "age": 20,
+#     "course": "B.Tech IT"
+# }
 
-print(student["name"])
+# print(student["name"])
 
-student["age"] = 21
-student["city"] = "Chennai"
+# student["age"] = 21
+# student["city"] = "Chennai"
 
-print(student.get("email", "Not available")) # 
+# print(student.get("email", "Not available")) # 
 
-for key, value in student.items():
-    print(key, ":", value)
+# for key, value in student.items():
+#     print(key, ":", value)
 
-# del student["city"]
-# print(student["city"])
+# # del student["city"]
+# # print(student["city"])
 
+
+# no name 
+
+
+numbers = [2, 5, 2, 8, 2, 5]
+targets = [2, 5, 8]
+
+for target in targets:
+    count = 0
+    for value in numbers:
+        if value == target:
+            count += 1
+    print(target, count)
